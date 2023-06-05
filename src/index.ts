655fcb23135e93b2e32047419a35558f76c376c8
Andreia Alferes <108821321+AndreiaAlferes@users.noreply.github.com>
2023-06-05 21:50:43 +0100

    Add logic to add a task and add Sass

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,63 @@
 import { v4 as uuidV4 } from "uuid";
 
 console.log(uuidV4());
+
+type Task = {
+  id: string;
+  title: string;
+  completed: boolean;
+  createdAt: Date;
+};
+
+const list = document.querySelector<HTMLUListElement>("#list");
+const form = document.getElementById("new-task-form") as HTMLFormElement | null;
+const input = document.querySelector<HTMLInputElement>("#new-task-title");
+
+//storage the tasks in the localStorage
+const tasks: Task[] = loadTasks();
+tasks.forEach(addListItem);
+
+//add advent listner to a form element
+form?.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if (input?.value == "" || input?.value == null) return;
+
+  const newTask: Task = {
+    id: uuidV4(),
+    title: input.value,
+    completed: false,
+    createdAt: new Date(),
+  };
+
+  tasks.push(newTask);
+
+  addListItem(newTask);
+  saveTasks();
+  input.value = "";
+});
+
+function addListItem(task: Task) {
+  const item = document.createElement("li");
+  const label = document.createElement("label");
+  const checkbox = document.createElement("input");
+  checkbox.addEventListener("change", (e) => {
+    task.completed = checkbox.checked;
+    saveTasks();
+  });
+  checkbox.type = "checkbox";
+  checkbox.checked = task.completed;
+  label.append(checkbox, task.title);
+  item.append(label);
+  list?.append(item);
+}
+
+function saveTasks() {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function loadTasks(): Task[] {
+  const taskJSON = localStorage.getItem("TASKS");
+  if (taskJSON == null) return [];
+  return JSON.parse(taskJSON);
+}
